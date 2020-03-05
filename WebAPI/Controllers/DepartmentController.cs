65af070464bc5375ced956e113f99e0c8ba2f60e
pradnyajosh <pradnyajosh@cybage.com>
2020-03-05 15:07:27 +0530

    initial commit to local repo

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,62 @@
+﻿namespace WebAPI.Controllers
+{
+    using EntityModelLib;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http;
+    public class DepartmentController : ApiController
+    {
+        //EntityModel context = new EntityModel();
+
+        public IHttpActionResult Get()
+        {
+            using (var context = new EntityModel())
+            {
+                var departments = context.Departments;
+                return Ok(departments);
+            }
+        }
+        public IHttpActionResult Get(int id)
+        {
+            using (var context = new EntityModel())
+            {
+                Department department = (from d in context.Departments.Include("Employees")
+                                         where d.DepartmentId == id
+                                         select d).FirstOrDefault();
+                if (department == null)
+                {
+                    return NotFound();
+                }
+                return Ok(department);
+            }
+        }
+
+        public IHttpActionResult Post(Department department)
+        {
+            if (department != null)
+            {
+                using (var context = new EntityModel())
+                {
+                    context.Departments.Add(department);
+                    context.SaveChanges();
+                    return CreatedAtRoute("DefaultApi", new { id = department.DepartmentId }, department);
+                }
+            }
+            return BadRequest();
+        }
+        public IHttpActionResult Put(Department department)
+        {
+            if (department != null)
+            {
+                // Do some work .
+                return Content(HttpStatusCode.Accepted, department);
+            }
+            return BadRequest();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            //Do some work
+            return Ok(true);
+        }
+    }
+}
---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿namespace WebAPI.Controllers
+{
+    using EntityModelLib;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    public class EmployeeController : ApiController
+    {
+        EntityModel context = new EntityModel();
+
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, context.Employees);
+        }
+        public HttpResponseMessage Get(int id)
+        {
+
+            var employee = context.Employees.Where(p => p.Id == id).FirstOrDefault();
+            if (employee == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+
+        public HttpResponseMessage Post(Employee employee)
+        {
+            if (employee == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            employee = context.Employees.Add(employee);
+            context.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
